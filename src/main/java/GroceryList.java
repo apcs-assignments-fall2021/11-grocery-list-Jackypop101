941a93a45c1e7f6d4ea2e7d727f7166b965edd97
Jack Chen <23hchen@athenian.org>
2021-11-04 23:29:32 -0700

    Does not work, help plz.

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -5,19 +5,29 @@ public class GroceryList {
     // Remember to create the array!
     // You don't need to change any of the values in the array
     public GroceryList() {
-        // YOUR CODE HERE
+        String[] arr = new String[10];
     }
 
     // Adds an item to the grocery list
     public void add(String item) {
-        // YOUR CODE HERE
+        for (int i = 0; i < 10; i++){
+            if (arr[i] == null){
+                arr[i] = item;
+                break;
+            }
+        }
     }
 
     // Removes an item from the grocery list
     // Replaces the item with null
     // Remember to shift anything to the left if necessary
     public void remove(String item) {
-        // YOUR CODE HERE
+        for (int i = 0; i < 10; i++){
+            if (arr[i] == item){
+                arr[i] = null;
+                break;
+            }
+        }
     }
 
     // Returns a String representation the grocery list
@@ -29,7 +39,12 @@ public class GroceryList {
     // You **may** have an extra comma at the end
     @Override
     public String toString() {
-        // YOUR CODE HERE
-        return "";
+        String w = "";
+        for (int i = 0; i < 10; i++){
+            if (arr[i] != null){
+                w += " " + arr[i] + ",";
+            }
+        }
+        return "Grocery List:" + w;
     }
 }
